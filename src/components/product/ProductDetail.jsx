231a9cff4a0fc33d57fd75c465f1dd1b3098912c
Muthu-AMMEW <muthu.ammew@gmail.com>
2025-04-18 19:30:55 +0530

    updates

--- src/components/product/ProductDetail.jsx
+++ src/components/product/ProductDetail.jsx
@@ -6,14 +6,14 @@ import Loader from '../layouts/Loader';
 import { Carousel } from 'react-bootstrap';
 import MetaData from "../layouts/MetaData";
 import { addCartItem } from "../../actions/cartActions";
-import {clearReviewSubmitted, clearError, clearProduct} from '../../slices/productSlice';
-import {Modal} from 'react-bootstrap';
+import { clearReviewSubmitted, clearError, clearProduct } from '../../slices/productSlice';
+import { Modal } from 'react-bootstrap';
 import { toast } from "react-toastify";
 import ProductReview from "./ProductReview";
 
 
-export default function ProductDetail () {
-    const { loading, product = {}, isReviewSubmitted, error} = useSelector((state)=>state.productState);
+export default function ProductDetail() {
+    const { loading, product = {}, isReviewSubmitted, error } = useSelector((state) => state.productState);
     const { user } = useSelector(state => state.authState);
     const dispatch = useDispatch();
     const { id } = useParams()
@@ -21,13 +21,13 @@ export default function ProductDetail () {
 
     const increaseQty = () => {
         const count = document.querySelector('.count')
-        if(product.stock ==0 ||  count.valueAsNumber >= product.stock) return;
+        if (product.stock == 0 || count.valueAsNumber >= product.stock) return;
         const qty = count.valueAsNumber + 1;
         setQuantity(qty);
     }
     const decreaseQty = () => {
         const count = document.querySelector('.count')
-        if(count.valueAsNumber == 1 ) return;
+        if (count.valueAsNumber == 1) return;
         const qty = count.valueAsNumber - 1;
         setQuantity(qty);
     }
@@ -48,146 +48,146 @@ export default function ProductDetail () {
 
     }
 
-    useEffect(()=>{
-        if(isReviewSubmitted) {
+    useEffect(() => {
+        if (isReviewSubmitted) {
             handleClose()
-            toast('Review Submitted successfully',{
+            toast('Review Submitted successfully', {
                 type: 'success',
                 position: toast.POSITION.BOTTOM_CENTER,
                 onOpen: () => dispatch(clearReviewSubmitted())
             })
-            
+
         }
-        if(error)  {
+        if (error) {
             toast(error, {
                 position: toast.POSITION.BOTTOM_CENTER,
                 type: 'error',
-                onOpen: ()=> { dispatch(clearError()) }
+                onOpen: () => { dispatch(clearError()) }
             })
             return
         }
-        if(!product._id || isReviewSubmitted) {
+        if (!product._id || isReviewSubmitted) {
             dispatch(getProduct(id))
         }
 
         return () => {
             dispatch(clearProduct())
         }
-        
 
-    },[dispatch,id,isReviewSubmitted, error])
+
+    }, [dispatch, id, isReviewSubmitted, error])
 
 
 
     return (
         <>
-            {loading? <Loader/>:
-            <>
-                <MetaData title={product.name} />
-                <div className="row f-flex justify-content-around">
-                    <div className="col-12 col-lg-5 img-fluid" id="product_image">
-                        <Carousel pause="hover">
-                            {product.images && product.images.length > 0 && product.images.map(image =>
-                                <Carousel.Item key={image._id}>
-                                    <img className="d-block w-100"  src={image.image} alt={product.name} height="500" width="500" />
-                                </Carousel.Item>
-                            )}
-                        </Carousel>
-                    </div>
-
-                    <div className="col-12 col-lg-5 mt-5">
-                    <h3>{product.name}</h3>
-                    <p id="product_id">Product # {product._id}</p>
-
-                    <hr/>
-
-                    <div className="rating-outer">
-                        <div className="rating-inner" style={{width: `${product.ratings/ 5 * 100}%` }}></div>
-                    </div>
-                    <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
-
-                    <hr/>
-
-                    <p id="product_price">${product.price}</p>
-                    <div className="stockCounter d-inline">
-                        <span className="btn btn-danger minus" onClick={decreaseQty} >-</span>
-
-                        <input type="number" className="form-control count d-inline" value={quantity} readOnly />
-
-                        <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
-                    </div>
-                    <button type="button" id="cart_btn" 
-                     disabled={product.stock==0?true:false} 
-                     onClick={()=>{
-                        dispatch(addCartItem(product._id, quantity))
-                        toast('Cart Item Added!',{
-                            type: 'success',
-                            position: toast.POSITION.BOTTOM_CENTER
-                        })
-                    }}
-                     className="btn btn-primary d-inline ml-4"
-                     >Add to Cart</button>
-
-                    <hr/>
-
-                    <p>Status: <span className={product.stock > 0 ?'greenColor':'redColor'} id="stock_status">{ product.stock > 0 ?'In Stock':'Out of Stock'}</span></p>
-
-                    <hr/>
-
-                    <h4 className="mt-2">Description:</h4>
-                    <p>{product.description}</p>
-                    <hr/>
-                    <p id="product_seller mb-3">Sold by: <strong>{product.seller}</strong></p>
-                    { user ?
-                    <button onClick={handleShow} id="review_btn" type="button" className="btn btn-primary mt-4" data-toggle="modal" data-target="#ratingModal">
-                                Submit Your Review
-                    </button> :
-                    <div className="alert alert-danger mt-5"> Login to Post Review</div>
-                    }
-                    
-                    <div className="row mt-2 mb-5">
-                        <div className="rating w-50">
-                            <Modal show={show} onHide={handleClose}>
-                                <Modal.Header closeButton>
-                                <Modal.Title>Submit Review</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body>
-                                    <ul className="stars" >
-                                        {
-                                            [1,2,3,4,5].map(star => (
-                                                <li 
-                                                value={star}
-                                                onClick={()=>setRating(star)}
-                                                className={`star ${star<=rating?'orange':''}`}
-                                                onMouseOver={(e) => e.target.classList.add('yellow')}
-                                                onMouseOut={(e) => e.target.classList.remove('yellow')}
-
-                                                ><i className="fa fa-star"></i></li>
-                                            ))
-                                        }
-                                       
-                                       
-                                    </ul>
-
-                                    <textarea  onChange={(e)=>setComment(e.target.value)} name="review" id="review" className="form-control mt-3">
-
-                                    </textarea>
-                                    <button disabled={loading} onClick={reviewHandler}   aria-label="Close" className="btn my-3 float-right review-btn px-4 text-white">Submit</button>
-                                </Modal.Body>
-                               
-                            </Modal>
+            {loading ? <Loader /> :
+                <>
+                    <MetaData title={product.name} />
+                    <div className="row f-flex justify-content-around">
+                        <div className="col-12 col-lg-5 img-fluid" id="product_image">
+                            <Carousel pause="hover">
+                                {product.images && product.images.length > 0 && product.images.map(image =>
+                                    <Carousel.Item key={image._id}>
+                                        <img className="d-block w-100" src={image.image} alt={product.name} height="500" width="500" />
+                                    </Carousel.Item>
+                                )}
+                            </Carousel>
                         </div>
+
+                        <div className="col-12 col-lg-5 mt-5">
+                            <h3>{product.name}</h3>
+                            <p id="product_id">Product # {product._id}</p>
+
+                            <hr />
+
+                            <div className="rating-outer">
+                                <div className="rating-inner" style={{ width: `${product.ratings / 5 * 100}%` }}></div>
+                            </div>
+                            <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
+
+                            <hr />
+
+                            <p id="product_price">${product.price}</p>
+                            <div className="stockCounter d-inline">
+                                <span className="btn btn-danger minus" onClick={decreaseQty} >-</span>
+
+                                <input type="number" className="form-control count d-inline" value={quantity} readOnly />
+
+                                <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
+                            </div>
+                            <button type="button" id="cart_btn"
+                                disabled={product.stock == 0 ? true : false}
+                                onClick={() => {
+                                    dispatch(addCartItem(product._id, quantity))
+                                    toast('Cart Item Added!', {
+                                        type: 'success',
+                                        position: toast.POSITION.BOTTOM_CENTER
+                                    })
+                                }}
+                                className="btn btn-primary d-inline ml-4"
+                            >Add to Cart</button>
+
+                            <hr />
+
+                            <p>Status: <span className={product.stock > 0 ? 'greenColor' : 'redColor'} id="stock_status">{product.stock > 0 ? 'In Stock' : 'Out of Stock'}</span></p>
+
+                            <hr />
+
+                            <h4 className="mt-2">Description:</h4>
+                            <p>{product.description}</p>
+                            <hr />
+                            <p id="product_seller mb-3">Sold by: <strong>{product.seller}</strong></p>
+                            {user ?
+                                <button onClick={handleShow} id="review_btn" type="button" className="btn btn-primary mt-4" data-toggle="modal" data-target="#ratingModal">
+                                    Submit Your Review
+                                </button> :
+                                <div className="alert alert-danger mt-5"> Login to Post Review</div>
+                            }
+
+                            <div className="row mt-2 mb-5">
+                                <div className="rating w-50">
+                                    <Modal show={show} onHide={handleClose}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Submit Review</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <ul className="stars" >
+                                                {
+                                                    [1, 2, 3, 4, 5].map(star => (
+                                                        <li
+                                                            value={star}
+                                                            onClick={() => setRating(star)}
+                                                            className={`star ${star <= rating ? 'orange' : ''}`}
+                                                            onMouseOver={(e) => e.target.classList.add('yellow')}
+                                                            onMouseOut={(e) => e.target.classList.remove('yellow')}
+
+                                                        ><i className="fa fa-star"></i></li>
+                                                    ))
+                                                }
+
+
+                                            </ul>
+
+                                            <textarea onChange={(e) => setComment(e.target.value)} name="review" id="review" className="form-control mt-3">
+
+                                            </textarea>
+                                            <button disabled={loading} onClick={reviewHandler} aria-label="Close" className="btn my-3 float-right review-btn px-4 text-white">Submit</button>
+                                        </Modal.Body>
+
+                                    </Modal>
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
-                            
-                </div>
 
-                </div>
-
-                {
-                product.reviews && product.reviews.length > 0 ?
-                <ProductReview reviews={product.reviews} /> : null
-                }
-            </>}
+                    {
+                        product.reviews && product.reviews.length > 0 ?
+                            <ProductReview reviews={product.reviews} /> : null
+                    }
+                </>}
         </>
     )
 }
--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,10 +1,10 @@
-import {useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearAuthError, login } from '../../actions/userActions';
 import MetaData from '../layouts/MetaData';
 import { toast } from 'react-toastify';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
- export default function Login() {
+export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const dispatch = useDispatch();
@@ -12,66 +12,66 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
     const location = useLocation();
 
     const { loading, error, isAuthenticated } = useSelector(state => state.authState)
-    const redirect = location.search?'/'+location.search.split('=')[1]:'/';
+    const redirect = location.search ? '/' + location.search.split('=')[1] : '/';
 
-    const  submitHandler = (e) => {
+    const submitHandler = (e) => {
         e.preventDefault();
         dispatch(login(email, password))
     }
 
     useEffect(() => {
-        if(isAuthenticated) {
+        if (isAuthenticated) {
             navigate(redirect)
         }
 
-        if(error)  {
+        if (error) {
             toast(error, {
                 position: toast.POSITION.BOTTOM_CENTER,
                 type: 'error',
-                onOpen: ()=> { dispatch(clearAuthError) }
+                onOpen: () => { dispatch(clearAuthError) }
             })
             return
         }
-    },[error, isAuthenticated, dispatch, navigate])
+    }, [error, isAuthenticated, dispatch, navigate])
 
     return (
         <>
             <MetaData title={`Login`} />
-            <div className="row wrapper"> 
+            <div className="row wrapper">
                 <div className="col-10 col-lg-5">
                     <form onSubmit={submitHandler} className="shadow-lg">
                         <h1 className="mb-3">Login</h1>
                         <div className="form-group">
-                        <label htmlFor="email_field">Email</label>
-                        <input
-                            type="email"
-                            id="email_field"
-                            className="form-control"
-                            value={email}
-                            onChange={e =>setEmail(e.target.value)}
-                        />
+                            <label htmlFor="email_field">Email</label>
+                            <input
+                                type="email"
+                                id="email_field"
+                                className="form-control"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
                         </div>
-            
+
                         <div className="form-group">
-                        <label htmlFor="password_field">Password</label>
-                        <input
-                            type="password"
-                            id="password_field"
-                            className="form-control"
-                            value={password}
-                            onChange={e =>setPassword(e.target.value)}
-                        />
+                            <label htmlFor="password_field">Password</label>
+                            <input
+                                type="password"
+                                id="password_field"
+                                className="form-control"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
                         </div>
 
                         <Link to="/password/forgot" className="float-right mb-4">Forgot Password?</Link>
-            
+
                         <button
-                        id="login_button"
-                        type="submit"
-                        className="btn btn-block py-3"
-                        disabled={loading}
+                            id="login_button"
+                            type="submit"
+                            className="btn btn-block py-3"
+                            disabled={loading}
                         >
-                        LOGIN
+                            LOGIN
                         </button>
 
                         <Link to="/register" className="float-right mt-3">New User?</Link>
