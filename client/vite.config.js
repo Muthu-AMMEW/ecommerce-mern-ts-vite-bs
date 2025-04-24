50b2566df188aed92572154adadedf5586b242a3
Muthu-AMMEW <muthu.ammew@gmail.com>
2025-04-24 21:57:57 +0530

    Merge Backend and frontend as single app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa';
+
+const env = loadEnv('development', process.cwd())
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -28,10 +30,15 @@ export default defineConfig({
         ]
       }
     })
-  ], server: {
+  ],
+  define: {
+    // Access your env variables directly
+    __API_URL__: JSON.stringify(env.VITE_API_URL),
+  },
+  server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // your backend
+        target: env.VITE_API_URL, // your backend
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
