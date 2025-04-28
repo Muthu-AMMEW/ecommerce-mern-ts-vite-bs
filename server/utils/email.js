c7120e80ca8f7647ceb29fe11cd17fcda3cfb454
Muthu-AMMEW <muthu.ammew@gmail.com>
2025-04-28 21:13:33 +0530

    Mail Trap updates

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,25 +1,24 @@
-const nodemailer = require('nodemailer')
+const Nodemailer = require("nodemailer");
+const { MailtrapTransport } = require("mailtrap");
 
 const sendEmail = async options => {
-    const transport = {
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    };
+    const transport = Nodemailer.createTransport(
+        MailtrapTransport({
+          token: process.env.MAIL_TRAP_TOKEN,
+        })
+      );
 
-    const transporter = nodemailer.createTransport(transport);
+      const sender = {
+        address: `<${process.env.SMTP_FROM_EMAIL}>`,
+        name: `${process.env.SMTP_FROM_NAME}`,
+      };
 
-    const message = {
-        from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+    await transport.sendMail({
+        from: sender,
         to: options.email,
         subject: options.subject,
         text: options.message
-    }
-
-    await transporter.sendMail(message)
+    })
 }
 
 module.exports = sendEmail
