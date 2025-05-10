46088786453b15c626b403708343fe6912d18064
Muthu-AMMEW <muthu.ammew@gmail.com>
2025-05-10 18:28:01 +0530

    forgot password updated

--- client/src/components/user/ForgotPassword.jsx
+++ client/src/components/user/ForgotPassword.jsx
@@ -4,16 +4,48 @@ import { toast } from "react-toastify";
 import { forgotPassword, clearAuthError } from "../../actions/userActions";
 
 export default function ForgotPassword() {
-    const [email, setEmail] = useState("");
+    const [inputs, setInputs] = useState({
+        email: ""
+    })
+    const initialStateErrors = {
+        email: false,
+        custom_error: null
+    };
+    const [errors, setErrors] = useState(initialStateErrors);
     const dispatch = useDispatch();
-    const { error, message } = useSelector(state => state.authState);
+    const { error, message, loading } = useSelector(state => state.authState);
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('email', email);
-        dispatch(forgotPassword(formData))
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
     }
+
+    function handleReset() {
+        setInputs({
+            email: ""
+        })
+        toast.info("Reset Successfully");
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let errors = initialStateErrors;
+        let hasError = false;
+        if (inputs.email === "") {
+            errors.email = true;
+            hasError = true;
+        }
+
+        if (!hasError) {
+            const formData = new FormData();
+            formData.append('email', inputs.email);
+            dispatch(forgotPassword(formData))
+        }
+        setErrors(errors);
+    }
+
 
     useEffect(() => {
         if (message) {
@@ -21,7 +53,9 @@ export default function ForgotPassword() {
                 type: 'success',
                 position: toast.POSITION.BOTTOM_CENTER
             })
-            setEmail("");
+            setInputs({
+                email: ""
+            })
             return;
         }
 
@@ -37,30 +71,46 @@ export default function ForgotPassword() {
 
 
     return (
-        <div className="row wrapper">
-            <div className="col-10 col-lg-5">
-                <form onSubmit={submitHandler} className="shadow-lg">
-                    <h1 className="mb-3">Forgot Password</h1>
-                    <div className="form-group">
-                        <label htmlFor="email_field">Enter Email</label>
-                        <input
-                            type="email"
-                            id="email_field"
-                            className="form-control"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
+        <>
+            <div className="row min-vw-100 min-vh-100 justify-content-center align-items-center mm-bgpic">
+                <div className="col-11 col-sm-8 col-md-7 col-lg-6 col-xl-5">
+
+                    <div className="d-flex flex-column justify-content-center align-items-center w-100 p-5 rounded-5 bg-body-tertiary bg-opacity-50">
+                        <div className='text-center h2'>Reset Password</div>
+                        <form className="w-100" onSubmit={handleSubmit}>
+
+                            <div className="w-100 mt-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input type="email" className="form-control" id="email" value={inputs.email} onChange={handleChange} placeholder="Enter email" name="email" />
+                                {errors.email ?
+                                    (<span className="text-danger bg-warning-subtle" >
+                                        Email Address is required.
+                                    </span>) : null
+                                }
+                            </div>
+
+
+                            <div className="mt-3 text-center">
+                                <span>
+                                    {errors.custom_error ?
+                                        (<p className="text-danger bg-warning-subtle rounded-5">{errors.custom_error}</p>)
+                                        : null
+                                    }
+                                </span>
+                                {loading ?
+                                    (<div className="text-center">
+                                        <div className="spinner-border text-primary " role="status">
+                                        </div>
+                                    </div>) : null
+                                }
+
+                                <button className="btn btn-primary me-5" type="submit">Submit</button>
+                                <button className="btn btn-danger" type="reset" onClick={handleReset}>Reset</button>
+                            </div>
+                        </form>
                     </div>
-
-                    <button
-                        id="forgot_password_button"
-                        type="submit"
-                        className="btn btn-block py-3">
-                        Send Email
-                    </button>
-
-                </form>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
